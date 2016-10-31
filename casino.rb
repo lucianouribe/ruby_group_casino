d2707a533d38ded33c538ef98e39d558c2f80239
Luciano Uribe <lucianouribe@gmail.com>
2016-10-31 15:54:15 -0600

    Multiple players and bank rolling

--- casino.rb
+++ casino.rb
@@ -8,8 +8,40 @@ class Casino
   attr_accessor :player
 
   def initialize
+    @players = []
     puts "Welcome to the Ruby Casino!".colorize(:blue)
-    @player = Player.new
+    users
+  end
+
+  def users
+    puts "[1] New Player"
+    puts "[2] Existing Player"
+    choice = gets.strip
+    if choice == '1'
+      if @players.count <= 5
+        @player = Player.new
+        @players << @player
+      else
+        puts "This is not a stadium, it's a gambling game, just enough room for five! Too late! Go away!"
+      end
+    else
+      @players.each_with_index { |x, i| puts "#{i + 1} #{x.name} #{x.bank_roll}" }
+      puts "Choose player"
+      pick_existing_player = gets.strip
+      case pick_existing_player
+        when '1'
+          @player = @players[0]
+        when '2'
+          @player = @players[1]
+        when '3'
+          @player = @players[2]
+        when '4'
+          @player = @players[3]
+        when '5'
+          @player = @players[4]
+      end
+      puts "Ok #{@player.name} here we go!!!"
+    end
     menu
   end
 
@@ -19,7 +51,8 @@ class Casino
 
     puts "[1] Highs and Lows"
     puts "[2] Slots"
-    puts "[3] Exit"
+    puts "[3] Players"
+    puts "[4] Exit"
     choice = gets.strip
     case choice
       when '1'
@@ -27,6 +60,8 @@ class Casino
       when '2'
         Slots.new(player)
       when '3'
+        users
+      when '4'
         exit
       else
         puts "Learn how to write"
--- player.rb
+++ player.rb
@@ -1,5 +1,8 @@
 class Player
   attr_accessor :name, :bank_roll
+  # @player_array = [
+  #   {name: player, bank_roll: bank_roll, games_won: number, games_lost: number}
+  # ]
   def initialize
       puts "What's your name player?"
       @name = gets.strip
@@ -11,9 +14,19 @@ class Player
       # Do we want bank roll to be a flaot?
       # Player to have a wallet
       # When the player gets created, it gets a new wallet instance (wallet be own class)
-      puts "How much money do you want to loose?"
+      puts "How much money do you have in your wallet?"
       choice = gets.strip.to_i
       @bank_roll = choice
   end
 
+  # def new_player
+  #   puts "What's your name player?"
+  #   Player.new = gets.strip
+  #   puts "How much money do you want to loose?"
+  #   choice = gets.strip.to_i
+  #   @bank_roll = choice
+  # end
+
 end
+
+# array_of_hash << { first_name: 'Marcos'}
